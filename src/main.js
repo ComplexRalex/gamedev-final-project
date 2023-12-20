ac58b4cfa9d2e706b20ac99dea44920ce26467c5
ComplexRalex <ralexbatres@gmail.com>
2023-12-19 20:57:07 -0600

    added favicon XD

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import Dialog from './scenes/Dialog.js';
 
 const config = {
     title: "proyecto_final",
-    version: "1.0.5",
+    version: "1.0.6",
     type: Phaser.AUTO,
     scale: {
         parent: "phaser_container",
